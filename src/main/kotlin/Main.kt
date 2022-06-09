1f7bc6db87e8d96713092d1a7e8b180c58275f4b
Joao Castro <cjoao.contato@gmail.com>
2022-06-08 21:20:13 -0300

    Iniciando Curso de Kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+/**
+ * Type     Bit
+ * Double   64
+ * Float    32
+ * Long     64
+ * Int      32
+ * Short    16
+ * Byte     8
+ * Boolean  ?
+ * String   ?
+ * Char     ?
+ */
+
+fun main() {
+    println("Iniciando com Kotlin")
+
+    val nome = "João Castro"
+
+    println(nome)
+
+}
